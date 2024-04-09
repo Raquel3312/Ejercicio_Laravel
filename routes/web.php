c1e3a3fb90a7fb1240e151112171fd80e2af414c
Raquel3312 <agarciap31@miumg.edu.gt>
2024-04-09 10:26:36 -0600

    cambios en controlador

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\HomeController;
 
-Route::get('/home', function () {
-    return view('home');
+
+Route::get('/', HomeController::class);
+
+
+
+/*Route::get('cursos', function() {
+    return "bienvenido a la pagina cursos";
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('cursos/create', function () {
+    return "en esta pagina podras crear un curso";
 });
 
-route::get('test',[HomeController::class,'index']);
+Route::get('cursos/{curso}', function($curso){
+    return "bienvenido al curso: $curso";
+});
+
+/*Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if($categoria){
+        return "bienvenido al cruso $curso, de la categoria $categoria";
+    }else{
+        return "bienvenido al curso: $curso";
+    }
+   });
+
+*/
+//route::get('test',[HomeController::class,'index']);*/
