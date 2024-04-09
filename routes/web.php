b6af95a511639a17faaac0a7769ad652c818131f
Raquel3312 <agarciap31@miumg.edu.gt>
2024-04-08 21:25:51 -0600

    Agregue controlador a la vista

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route::get('test',[HomeController::class,'index']);
